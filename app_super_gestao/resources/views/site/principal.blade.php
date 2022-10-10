a5924cb71211649eb0cea863de87bf02035a389f
johnerick-py <jhonerick147@outlook.com>
2022-10-10 20:21:14 -0300

    alterando views

--- app_super_gestao/resources/views/site/principal.blade.php
+++ app_super_gestao/resources/views/site/principal.blade.php
@@ -5,7 +5,7 @@
         <a href="{{ route('site.index') }}">Principal</a>
     </li>
     <li>
-        <a href="{{ route('site.sobrenos') }}">Sobre nós</a>
+        <a href="{{ route('site.sobre') }}">Sobre nós</a>
     </li>
     <li>
         <a href="{{ route('site.contato') }}">Contato</a>
