b0d624dda0ffbec5bd9030923859413fe68c1374
johnerick-py <jhonerick147@outlook.com>
2022-10-10 20:12:14 -0300

    alterando sobre

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 */
 
 Route::get('/', 'PrincipalController@principal')->name('site.index');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/sobre', 'SobreController@sobre')->name('site.sobre');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
